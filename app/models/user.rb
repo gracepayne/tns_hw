33734272ee6e2c1f5693a102a8dd877333f4fbc4
Grace Payne <grace.payne@tnsatlanta.org>
2016-03-30 17:08:16 -0400

    added validation for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, format: { with: /.*@tnsatlanta.org$/,
+    message: "must have a @tnsatlanta.org email address" , multiline: true}
 end
